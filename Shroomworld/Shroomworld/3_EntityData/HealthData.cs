ab15006b441630f9225f33e8639b8076ea7ef9cb
wero-wal <111140005+wero-wal@users.noreply.github.com>
2022-11-25 22:54:40 +0000

    1. Added ref readonlyHealthData to HealthData 2. Added a few simple methods to HealthData

--- Shroomworld/Shroomworld/3_EntityData/HealthData.cs
+++ Shroomworld/Shroomworld/3_EntityData/HealthData.cs
@@ -6,39 +6,49 @@ namespace Shroomworld
     {
         public int Health { get => _health; }
 
-
+// todo: percentage health
         protected int _health;
+        private ReadonlyHealthData _readonlyData;
+
+        private DateTime _healed;
 
 
-        ///// <summary>
-        ///// Decreases the health by the desired amount.
-        ///// </summary>
-        ///// <param name="amount"></param>
-        ///// <returns>true if health &lt;= 0</returns>
-        //public virtual void DecreaseHealth(int amount, out bool dead)
-        //{
-        //    _health -= amount;
-        //    dead = _health <= 0;
-        //}
+        public HealthData(int? health, ref ReadonlyHealthData readonlyHealthData)
+        {
+            _health = health ?? readonlyHealthData.MaxHealth;
+            _readonlyData = readonlyHealthData;
+        }
+
+        /// <summary>
+        /// Decreases the health by the desired amount.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns>true if health &lt;= 0</returns>
+        public bool DecreaseHealth(int amount)
+        {
+            _health -= amount;
+            return _health <= 0; // true => dead
+        }
         //public virtual void ResetHealth()
         //{
         //    _health = _maxHealth;
         //}
-        //public virtual void RegenerateHealth(int fps)
-        //{
-        //    if (_health < _maxHealth)
-        //    {
-        //        int healAmount = _regenAmountPerSecond / fps;
-        //        AddHealth(healAmount);
-        //    }
-        //}
-        //public void AddHealth(int healthToAdd)
-        //{
-        //    _health += healthToAdd;
-        //}
-        //public virtual string ToString()
-        //{
-        //    return FileFormatter.FormatAsPlainText(_health, _maxHealth, _regenAmountPerSecond, levelOfSeparator: FileFormatter.Secondary);
-        //}
+        public virtual void RegenerateHealth(int fps)
+        {
+            if((DateTime.Now - _healed).TotalSeconds >= 1)
+            {
+                AddHealth(_readonlyData.RegenAmountPerSecond);
+                //or just divide by fps. idk.
+                //or, change regen amount per second to regenCooldown
+            }
+        }
+        public void AddHealth(int healthToAdd)
+        {
+            _health = Math.Min(_health + healthToAdd, _readonlyData.MaxHealth);
+        }
+        public override string ToString()
+        {
+            return _health.ToString();
+        }
     }
 }
