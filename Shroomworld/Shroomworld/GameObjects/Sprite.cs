2ffe102c5afcabf32740c5c5df44a56f8cdfefad
werobot776 <105880258+werobot776@users.noreply.github.com>
2023-02-03 10:48:24 +0000

    feat: create Vertices class to store a Sprite's texture's vertices

--- Shroomworld/Shroomworld/GameObjects/Sprite.cs
+++ Shroomworld/Shroomworld/GameObjects/Sprite.cs
@@ -10,29 +10,19 @@ namespace Shroomworld
 {
     public class Sprite
     {
-        // ----- Enums -----
-        public class Vertices
-        {
-            public int TopLeft = 0;
-            public int TopRight = 1;
-            public int BottomLeft = 2;
-            public int BottomRight = 3;
-        }
-
-
         // ----- Properties -----
-        public Texture2D Texture { get => _texture; }
-        public Color Colour { get => _colour; }
+        public Texture2D Texture => _texture;
+        public Color Colour => _colour;
         public Vector2 Position => _position;
-        public Vector2 Size { get => _size; }
-
+        public Vector2 Size => _size;
+        public Vertices Vertices => _vertices;
 
         // ----- Fields -----
         protected Texture2D _texture;
         protected Color _colour;
         protected Vector2 _position;
         protected Vector2 _size;
-
+        protected Vertices _vertices;
 
         // ----- Constructors -----
         public Sprite(Texture2D texture)
@@ -41,27 +31,10 @@ namespace Shroomworld
             _size = new Vector2(_texture.Width, _texture.Height);
         }
 
-
         // ----- Methods -----
-        public Vector2[] GetVertices()
+        public void UpdateVertices()
         {
-            return new Vector2[]
-            {
-                _position, // Top Left
-                _position + _size * Vector2.UnitX, // Top Right
-                _position + _size * Vector2.UnitY, // Bottom Left
-                _position + _size, // Bottom Right
-            };
-        }
-        public Vector2[] GetVerticesAt(Vector2 position)
-        {
-            return new Vector2[]
-            {
-                position, // Top Left
-                position + _size * Vector2.UnitX, // Top Right
-                position + _size * Vector2.UnitY, // Bottom Left
-                position + _size, // Bottom Right
-            };
+            _vertices.Update(_position, _size);
         }
         public void Draw()
         {
