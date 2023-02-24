4a066d79595976c423734af882d16211c861b1f8
wero-wal <weronikawalczak2005@gmail.com>
2023-02-24 17:56:39 +0000

    refactor: incorporate IdentifyingData to IType

--- Shroomworld/Shroomworld/Types/IType.cs
+++ Shroomworld/Shroomworld/Types/IType.cs
@@ -2,8 +2,6 @@ namespace Shroomworld
 {
 	internal interface IType
 	{
-		public int Id { get; }
-		public string FullId { get => $"{GetType().ToString().Split('.')[1]}{Id}"; }
-		public string Name { get; }
+		IdentifyingData IdData { get; }
 	}
 }
