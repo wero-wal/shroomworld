4f7e415b05a3b8dfec0ac52121218981e4d18a38
werobot776 <105880258+werobot776@users.noreply.github.com>
2023-03-02 10:34:37 +0000

    feat: add DrawSprite()
    refactor: make public

--- Shroomworld/Shroomworld/Main/DisplayHandler.cs
+++ Shroomworld/Shroomworld/Main/DisplayHandler.cs
@@ -1,18 +1,17 @@
-namespace Shroomworld
-{
-	internal static class DisplayHandler : IDisplayHandler
-	{
-		public static void DrawTexture(Texture2 texture, Vector2 position, Color colour = Color.White)
-		{
-			Shroomworld.SpriteBatch.Draw(texture, position, colour);
-		}
-		public static void DrawText(string text, Vector2 position, Color colour)
-		{
-			// todo: draw text
-		}
-		public static void DrawRectangle(Vector2 size, Vector2 position, Color colour)
-		{
-			// todo: draw rectangle
-		}
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace Shroomworld;
+public static class DisplayHandler : IDisplayHandler {
+	public static void DrawTexture(Texture2D texture, Vector2 position, Color colour = Color.White) {
+		Shroomworld.SpriteBatch.Draw(texture, position, colour);
+	}
+	public static void DrawText(string text, Vector2 position, Color colour) {
+		// todo: draw text
+	}
+	public static void DrawRectangle(Vector2 size, Vector2 position, Color colour) {
+		// todo: draw rectangle
+	}
+	public static void DrawSprite(Sprite sprite) {
+		Shroomworld.SpriteBatch.Draw(sprite.Texture, sprite.Position, Color.White);
 	}
 }
--- Shroomworld/Shroomworld/Main/IDisplayHandler.cs
+++ Shroomworld/Shroomworld/Main/IDisplayHandler.cs
@@ -1,9 +1,9 @@
-namespace Shroomworld
-{
-	internal interface IDisplayHandler
-	{
-		void DrawTexture(Texture2 texture, Vector2 position, Color colour = Color.White);
-		void DrawText(string text, Vector2 position, Color colour);
-		void DrawRectangle(Vector2 size, Vector2 position, Color colour);
-	}
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace Shroomworld;
+public interface IDisplayHandler {
+	void DrawTexture(Texture2D texture, Vector2 position, Color colour = Color.White);
+	void DrawText(string text, Vector2 position, Color colour = Color.Black);
+	void DrawRectangle(Vector2 size, Vector2 position, Color colour);
+	void DrawSprite(Sprite sprite);
 }
