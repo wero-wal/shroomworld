c3cb1f305af1f980ba5c3380a689f8f7358a5971
W. Walczak <weronikawalczak2005@gmail.com>
2023-03-03 19:30:42 +0000

    refactor: change nullable types to Maybes

--- Shroomworld/Shroomworld/Types/ItemType.cs
+++ Shroomworld/Shroomworld/Types/ItemType.cs
@@ -4,33 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shroomworld
-{
-    public class ItemType {
-        private readonly IdData _idData;
-        private readonly ToolData? _toolData;
-        private readonly bool _stackable;
-        private readonly bool _placeable;
+namespace Shroomworld;
 
-        /// <summary>
-        /// Constructor for non-tool items
-        /// </summary>
-        public ItemType(IdData idData, bool stackable, bool placeable)
-        {
-            _idData = idData;
-            _stackable = stackable;
-            _placeable = placeable;
-            _toolData = null;
-        }
-        /// <summary>
-        /// Constructor for tool items
-        /// </summary>
-        public ItemType(IdData idData, ToolData toolData)
-        {
-            _idData = idData;
-            _toolData = toolData;
-            _stackable = false;
-            _placeable = false;
-        }
+public class ItemType { 
+    private readonly IdData _idData;
+    private readonly ToolData? _toolData;
+    private readonly bool _stackable;
+    private readonly bool _placeable;
+
+    /// <summary>
+    /// Constructor for non-tool items
+    /// </summary>
+    public ItemType(IdData idData, bool stackable, bool placeable) {
+        _idData = idData;
+        _stackable = stackable;
+        _placeable = placeable;
+        _toolData = null;
+    }
+    /// <summary>
+    /// Constructor for tool items
+    /// </summary>
+    public ItemType(IdData idData, ToolData toolData) {
+        _idData = idData;
+        _toolData = toolData;
+        _stackable = false;
+        _placeable = false;
     }
 }
