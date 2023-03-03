7acf647b46eda763d4ee7bf8a61de88844de461b
W. Walczak <weronikawalczak2005@gmail.com>
2023-03-03 20:52:26 +0000

    docs: add method summaries
    fix: Inputs --> Input.Inputs

--- Shroomworld/Shroomworld/UserInterface/KeyBinds.cs
+++ Shroomworld/Shroomworld/UserInterface/KeyBinds.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections.Generic;
 namespace Shroomworld;
 public class KeyBinds {
+
     // ----- Enums -----
     // ----- Properties -----
+
     // ----- Fields -----
-    private Dictionary<Inputs, Action<>> _bindings;
+    private Dictionary<Input.Inputs, Action> _bindings;
 
 
     // ----- Constructors -----
@@ -16,28 +19,39 @@ public class KeyBinds {
     /// <summary>
     /// Execute the functions bound to the active inputs.
     /// </summary>
-    public void ProcessInputs(Inputs[] inputs) {
-        foreach (Inputs input in inputs) {
+    public void ProcessInputs(Input.Inputs[] inputs) {
+        foreach (Input.Inputs input in inputs) {
             _bindings[input]();
         }
     }
+    /// <summary>
+    /// Store all the possible inputs in a dictionary and bind them to <see cref="DoNothing"/>.
+    /// </summary>
     public void Initialize() {
-        _bindings = new Dictionary<Inputs, Action<>>((int)Inputs.None);
-        for (int i = 0; i < (int)Inputs.None; i++) {
-            _bindings.Add((Inputs)i, DoNothing);
+        _bindings = new(capacity: (int)Input.Inputs.None);
+        for (int i = 0; i < (int)Input.Inputs.None; i++) {
+            _bindings.Add((Input.Inputs)i, DoNothing);
         }
     }
-    public void Add(Inputs input, Action<> onKeyPressed) {
+    /// <summary>
+    /// If an input is not bound to any action, bind it to the specified action.
+    /// </summary>
+    /// <param name="input">The input (key or mouse button press) you want to bind the action to.</param>
+    /// <param name="onKeyPressed">The action to which you want to bind the input.</param>
+    public void Add(Input.Inputs input, Action onKeyPressed) {
         if (_bindings[input] == DoNothing) {
             _bindings[input] = onKeyPressed;
         }
     }
-    public void Remove(Inputs input) {
+    /// <summary>
+    /// Binds the <paramref name="input"/> to the function <see cref="DoNothing"/>.
+    /// </summary>
+    /// <param name="input">The input whose action / binding you want to remove.</param>
+    public void Remove(Input.Inputs input) {
         _bindings[input] = DoNothing;
     }
     /// <summary>
     /// Called when a key is pressed but has not been bound.
     /// </summary>
-    private Action<> DoNothing() {
-    }
+    private void DoNothing() { }
 }
