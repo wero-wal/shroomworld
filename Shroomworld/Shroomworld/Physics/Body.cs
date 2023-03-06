805453bef8ce0c09689f24bfa19adf1346049055
W. Walczak <weronikawalczak2005@gmail.com>
2023-03-06 21:36:01 +0000

    feat: change SetAcceleration() to AddAcceleration() and add ResetAcceleration()

--- Shroomworld/Shroomworld/Physics/Body.cs
+++ Shroomworld/Shroomworld/Physics/Body.cs
@@ -46,19 +46,23 @@ namespace Shroomworld.Physics {
 			_sprite.SetPosition(_sprite.Position + _velocity);
 		}
 		/// <summary>
-		/// Sets acceleration but doesn't apply it (i.e. doesn't change any other values to reflect this).
+		/// Add gravity to the acceleration in the positive vertical direction.
 		/// </summary>
-		/// <param name="direction">
-		/// 	<para>Direction vector of the acceleration (excluding gravity).</para>
-		/// 	<para>Can be normalised or unnormalised. If normalised, remember to set a <paramref name="magnitude"/>.</para>
-		/// </param>
-		/// <param name="magnitude">
-		/// 	<para>Magnitude of the acceleration.</para>
-		/// 	<para>Default value: <see cref="DefaultMagnitude"/></para>
-		/// </param>
-		public void SetAcceleration(Vector2 direction) {
-			_acceleration = direction * PhysicsData.Acceleration;
+		public void AddGravity() {
 			_acceleration.Y += PhysicsData.Gravity;
+		}
+		/// <summary>
+		/// Add acceleration of magnitude <see cref="PhysicsData.Acceleration"/> in the given <paramref name="direction"/> (but don't apply it).
+		/// </summary>
+		/// <param name="direction">Direction of the acceleration.</param>
+		public void AddAcceleration(Vector2 direction) {
+			_acceleration += direction * PhysicsData.Acceleration;
+		}
+		/// <summary>
+		/// Set acceleration to zero.
+		/// </summary>
+		public void ResetAcceleration() {
+			_acceleration = Vector2.Zero;
 		}
 		/// <summary>
 		/// Changes the position of the body to move it out of the hitbox of the tile with which it has collided.
