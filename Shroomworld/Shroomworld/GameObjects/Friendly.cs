778dea002130acefdc533931f8c018e306300c72
W. Walczak <weronikawalczak2005@gmail.com>
2023-03-06 23:56:52 +0000

    feat: add constructor

--- Shroomworld/Shroomworld/GameObjects/Friendly.cs
+++ Shroomworld/Shroomworld/GameObjects/Friendly.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using Shroomworld.Physics;
 
-namespace Shroomworld; 
+namespace Shroomworld;
 public class Friendly : Entity {
     // ----- Enums -----
     // ----- Properties -----
     // ----- Fields -----
     // ----- Constructors -----
+    public Friendly(IType type, Sprite sprite, EntityHealthData healthData, Body body) : base(type, sprite, healthData, body) {
+    }
+
     // ----- Methods -----
 }
