66fa606c536fef9ddf137c0e58f0a6290f032e7b
W. Walczak <weronikawalczak2005@gmail.com>
2023-03-07 00:04:09 +0000

    fix: Vector --> Vector2
    refactor: make public

--- Shroomworld/Shroomworld/Physics/Physics.cs
+++ Shroomworld/Shroomworld/Physics/Physics.cs
@@ -1,38 +1,34 @@
-using System;
 using Microsoft.Xna.Framework;
-namespace Shroomworld.Physics
-{
-    internal class Physics
-    {
-        public static bool CheckForCollision(Body a, Body b, out Vector direction, out float depth)
-		{
-			// todo: do the separating axis theorem for a.Shape and b.Shape
-			direction = null;
-			depth = 1;
-			return false;
-		}
-		private static bool CheckUsingSepAxisTheorem(Vertices a, Vertices b, out Vector direction, out float depth)
-		{
-			// todo: paste code here
-			direction = null;
-			depth = 1;
-			return false;
-		}
+namespace Shroomworld.Physics;
 
-		public static void ResolveCollision(Body a, Body b, Vector direction, float depth, bool share)
-		{
-			a.Velocity *= -a.E; // maybe?
-			b.Velocity *= -b.E; // maybe?
+public class Physics {
+    public static bool CheckForCollision(Body a, Body b, out Vector2 direction, out float depth) {
+		// todo: do the separating axis theorem for a.Shape and b.Shape
+		direction = Vector2.Zero;
+		depth = 1;
+		return false;
+	}
+	private static bool CheckUsingSepAxisTheorem(Vertices a, Vertices b, out Vector2 direction, out float depth)
+	{
+		// todo: paste code here
+		direction = Vector2.Zero;
+		depth = 1;
+		return false;
+	}
 
-			if (share) // or is one of them is static
-			{
-				a.Position -= 0.5f * direction * depth;
-				b.Position += 0.5f * direction * depth;
-			}
-			else
-			{
-				a.Position -= direction * depth;
-			}
+	public static void ResolveCollision(Body a, Body b, Vector2 direction, float depth, bool share)
+	{
+		//a.Velocity *= -a.E; // maybe?
+		//b.Velocity *= -b.E; // maybe?
+
+		if (share) // or is one of them is static
+		{
+			a.ResolveCollision(direction, depth / 2);
+			b.ResolveCollision(direction, depth / 2);
 		}
+		else
+		{
+			a.ResolveCollision(direction, depth);
+		}
+	}
     }
-}
