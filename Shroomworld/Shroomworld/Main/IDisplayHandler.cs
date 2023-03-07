e230a35ba106a2f10fcacf14782aeb27039b0260
W. Walczak <weronikawalczak2005@gmail.com>
2023-03-07 00:07:48 +0000

    fix: remove default colours

--- Shroomworld/Shroomworld/Main/IDisplayHandler.cs
+++ Shroomworld/Shroomworld/Main/IDisplayHandler.cs
@@ -2,8 +2,8 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 namespace Shroomworld;
 public interface IDisplayHandler {
-	void DrawTexture(Texture2D texture, Vector2 position, Color colour = Color.White);
-	void DrawText(string text, Vector2 position, Color colour = Color.Black);
+	void DrawTexture(Texture2D texture, Vector2 position, Color colour);
+	void DrawText(string text, Vector2 position, Color colour);
 	void DrawRectangle(Vector2 size, Vector2 position, Color colour);
 	void DrawSprite(Sprite sprite);
 }
