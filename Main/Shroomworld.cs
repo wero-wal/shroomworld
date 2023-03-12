43d7590d9b4006c5cb11ff3394c7d3b0adc23bcf
wero-wal <weronikawalczak2005@gmail.com>
2023-03-12 18:45:52 +0000

    chore: remove using Microsoft.Xna.Framework.Graphics

--- Main/Shroomworld.cs
+++ Main/Shroomworld.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
 using Shroomworld.FileHandling;
 
 namespace Shroomworld;
