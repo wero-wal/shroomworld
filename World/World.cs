cf9c4288410efc5057a689ca9067ba361cc2b383
wero-wal <weronikawalczak2005@gmail.com>
2023-03-14 19:15:01 +0000

    feat: add player update code to `Update()`

--- World/World.cs
+++ World/World.cs
@@ -48,6 +48,8 @@ public class World {
     // ----- Methods -----
     public void Update() {
         _keyBinds.ProcessInputs(Input.CurrentInputs);
+        _player.Body.ApplyPhysics();
+        _player.Body.ResetAcceleration();
     }
     public void Draw() {
         Shroomworld.DisplayHandler.SetBackground(Color.CornflowerBlue);
@@ -60,6 +62,8 @@ public class World {
                 Shroomworld.DisplayHandler.Draw(((TileType)_map[x, y]).Texture, x, y);
             }
         }
+
+        Shroomworld.DisplayHandler.Draw(_player.Sprite);
     }
     private void SetKeyBinds() {
         _keyBinds = new KeyBinds();
@@ -67,6 +71,7 @@ public class World {
         _keyBinds.Add(Input.Inputs.A, PlayerMoveLeft);
         _keyBinds.Add(Input.Inputs.D, PlayerMoveRight);
         _keyBinds.Add(Input.Inputs.Space, PlayerJump);
+        _keyBinds.Add(Input.Inputs.S, PlayerMoveDown);
         /*_keyBinds.Add(Input.Inputs.Escape, OpenPauseMenu);
         _keyBinds.Add(Input.Inputs.Q, OpenQuestMenu);
         _keyBinds.Add(Input.Inputs.E, OpenInventory);
@@ -83,5 +88,8 @@ public class World {
     }
     private void PlayerMoveRight() {
         _player.Body.AddAcceleration(Vector2.UnitX);
+    }
+    private void PlayerMoveDown() {
+        _player.Body.AddAcceleration(Vector2.UnitY);
     }
 }
