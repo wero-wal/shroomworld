b2f045b3a09b3687c40d039152943a06c5b55e3f
werobot776 <105880258+werobot776@users.noreply.github.com>
2023-03-14 16:08:23 +0000

    refactor: edit summaries
    ensure XML summary tags are on setheir own parate lines

--- Components/HealthData.cs
+++ Components/HealthData.cs
@@ -1,11 +1,14 @@
 ﻿namespace Shroomworld {
-	// todo: put key in readme (Readonly prefix means the data is constant)
-	/// <summary>Data about the health of a type of entity.</summary>
+	/// <summary>
+	/// Data about the health of a type of entity.
+	/// </summary>
 	public class HealthData {
 
 		// ----- Properties -----
 		public int MaxHealth => _maxHealth;
-		/// <value>How much to increase the entity's health by each second.</value>
+		/// <value>
+		/// How much to increase the entity's health by each second.
+		/// </value>
 		public int RegenerationPerSecond => _regenerationPerSecond;
 
 
