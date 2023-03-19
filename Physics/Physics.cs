a05031bdefec68185ac45bb4b65c7cb1e3b15115
W. Walczak <weronikawalczak2005@gmail.com>
2023-03-19 17:47:11 +0000

    feat: add collision resolution code

--- Physics/Physics.cs
+++ Physics/Physics.cs
@@ -1,34 +1,61 @@
+using System;
 using Microsoft.Xna.Framework;
 namespace Shroomworld.Physics;
 
 public class Physics {
-    public static bool CheckForCollision(Body a, Body b, out Vector2 direction, out float depth) {
-		// todo: do the separating axis theorem for a.Shape and b.Shape
-		direction = Vector2.Zero;
-		depth = 1;
-		return false;
-	}
-	private static bool CheckUsingSepAxisTheorem(Rectangle a, Rectangle b, out Vector2 direction, out float depth)
-	{
-		// todo: paste code here
-		direction = Vector2.Zero;
-		depth = 1;
-		return false;
-	}
 
-	public static void ResolveCollision(Body a, Body b, Vector2 direction, float depth, bool share)
-	{
-		//a.Velocity *= -a.E; // maybe?
-		//b.Velocity *= -b.E; // maybe?
-
-		if (share) // or is one of them is static
-		{
-			a.ResolveCollision(direction, depth / 2);
-			b.ResolveCollision(direction, depth / 2);
-		}
-		else
-		{
-			a.ResolveCollision(direction, depth);
+	// ----- Methods -----
+	/// <summary>
+	/// Check for and resolve all collisions for a specific <paramref name="entity"/> for a given set of <paramref name="tiles"/>.
+	/// </summary>
+	/// <param name="entity"></param>
+	/// <param name="tiles">Tiles which may potentially intersect the entity.
+	/// (All the tiles in the area from the top left corner of the entity's hitbox to the bottom right.)</param>
+	public static void ResolveCollisions(Body entity, Rectangle[] tiles) {
+		Vector2 resolutionVector;
+		foreach(Rectangle tile in tiles) {
+			if (CheckIfIntersect(entity, tile, out resolutionVector)) {
+				entity.ResolveCollision(resolutionVector);
+			}
 		}
 	}
-    }
+	/// <summary>
+	/// Use the Separating Axis Theorem to check whether or not two tiles intersect.
+	/// </summary>
+	/// <param name="entity"></param>
+	/// <param name="tile"></param>
+	/// <param name="direction"></param>
+	/// <param name="depth"></param>
+	/// <returns></returns>
+	private static bool CheckIfIntersect (Body entity, Rectangle tile, out Vector2 resolutionVector) {
+		resolutionVector = Vector2.Zero;
+		bool intersects = false;
+
+		// Check if the ... of the entity's hitbox is within the tile.
+		// - left
+		if ((tile.Left < entity.Sprite.Hitbox.Left)
+		&& (entity.Sprite.Hitbox.Left < tile.Right)) {
+			resolutionVector.X += tile.Right - entity.Sprite.Hitbox.Left;
+			intersects = true;
+		}
+		// - right
+		else if ((tile.Left < entity.Sprite.Hitbox.Right)
+		&& (entity.Sprite.Hitbox.Right < tile.Right)) {
+			resolutionVector.X += tile.Left - entity.Sprite.Hitbox.Right;
+			intersects = true;
+		}
+		// - top
+		if ((tile.Top < entity.Sprite.Hitbox.Top)
+		&& (entity.Sprite.Hitbox.Top < tile.Bottom)) {
+			resolutionVector.Y += entity.Sprite.Hitbox.Top - tile.Top;
+			intersects = true;
+		}
+		// - bottom
+		else if ((tile.Top < entity.Sprite.Hitbox.Bottom)
+		&& (entity.Sprite.Hitbox.Bottom < tile.Bottom)) {
+			resolutionVector.Y += tile.Top - entity.Sprite.Hitbox.Bottom;
+			intersects = true;
+		}
+		return intersects;
+	}
+}
