1c23c31a816309cf1002cde02e916252e775ec86
wero-wal <weronikawalczak2005@gmail.com>
2023-03-23 14:08:26 +0000

    refactor: re-implement `IDisplayHandler`

--- UserInterface/IDisplayHandler.cs
+++ UserInterface/IDisplayHandler.cs
@@ -1,30 +1,24 @@
-using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Shroomworld;
 public interface IDisplayHandler {
+	Vector2 CentreOfScreen { get; }
 	Texture2D BlankTexture { get; }
 
-	void MoveCamera(Rectangle playerHitbox);
-	void UpdateCentreOfScreen();
-
-    void Begin();
-	void End();
-	void DrawText(string text, Vector2 position, Color colour);
-	void DrawRectangle(Vector2 size, Vector2 position, Color colour);
+	void Begin();
+	void BeginText();
+	Rectangle GetTileHitbox(int x, int y, Point textureSize);
+	void ClampToWorld(ref Rectangle hitbox, ref Vector2 position);
 	void Draw(Texture2D texture, Vector2 position, Color colour);
+	void DrawRectangle(Vector2 size, Vector2 position, Color colour);
 	void DrawSprite(Sprite sprite);
-	void DrawTile(Texture2D texture, int x, int y);
-
-	void ClampToScreen(ref Rectangle hitbox, ref Vector2 position);
-    int GetHeightInTiles(Texture2D texture);
-    Point GetTileCoords(Sprite sprite, Func<float, int> round);
-	Vector2 CalculateTilePosition(int x, int y, int heightInTiles);
-
-	Rectangle CalculateTileHitbox(int x, int y, Texture2D texture);
-
-    void SetBounds(int width, int height);
+	void DrawText(string text, Vector2 position, Color colour);
+	void DrawTile(int x, int y, Texture2D texture);
+	void End();
+	(Point TopLeft, Point BottomRight) GetTileCoords(Rectangle hitbox);
 	void SetBackground(Color colour);
+	void SetBounds(int width, int height);
 	void SetTitle(string title);
+	void Update(Vector2 playerPosition);
 }
