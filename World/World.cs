6ed680d8fd0a5ab09c383641fbec7be74d901d4f
wero-wal <weronikawalczak2005@gmail.com>
2023-03-23 14:14:38 +0000

    refactor: alter collisions method to support pre-emptive resolution
    refactor: add tile type dictionary and display handler as static fields

--- World/World.cs
+++ World/World.cs
@@ -19,6 +19,9 @@ public class World {
 
 
     // ----- Fields -----
+    private static IDisplayHandler _display;
+    private static Dictionary<int, TileType> _tileTypes;
+
     private readonly Map _map;
     private readonly List<Friendly> _friendlies;
     private readonly List<Enemy> _enemies;
@@ -50,23 +53,28 @@ public class World {
     }
 
     // ----- Methods -----
+    public static void SetUp(IDisplayHandler displayHandler, Dictionary<int, TileType> tileTypes) {
+        _display = displayHandler;
+        _tileTypes = tileTypes;
+    }
+
     public void Update() {
         _keyBinds.ProcessInputs(Input.CurrentInputs);
         ApplyPhysics();
     }
     public void Draw() {
-        Shroomworld.DisplayHandler.SetBackground(Color.CornflowerBlue);
+        _display.SetBackground(Color.CornflowerBlue);
 
         for (int x = 0; x < _map.Width; x++) {
             for (int y = 0; y < _map.Height; y++) {
                 if (_map[x, y] == TileType.AirId) {
                     continue;
                 }
-                Shroomworld.DisplayHandler.DrawTile(x, y, ((TileType)_map[x, y]).Texture);
+                _display.DrawTile(x, y, _tileTypes[_map[x, y]].Texture);
             }
         }
 
-        Shroomworld.DisplayHandler.DrawSprite(_player.Sprite);
+        _display.DrawSprite(_player.Sprite);
     }
 
     private void SetKeyBinds() {
@@ -85,45 +93,52 @@ public class World {
         _keyBinds.Add(Input.Inputs.N4, SwitchToHotbarSlot4);*/
     }
     private void PlayerJump() {
-        _player.Body.AddAcceleration(-Vector2.UnitY * _physics.Acceleration);
+        _player.Body.AddAcceleration(_physics.AccelerationUp);
     }
     private void PlayerMoveLeft() {
-        _player.Body.AddAcceleration(-Vector2.UnitX * _physics.Acceleration);
+        _player.Body.AddAcceleration(_physics.AccelerationLeft);
     }
     private void PlayerMoveRight() {
-        _player.Body.AddAcceleration(Vector2.UnitX * _physics.Acceleration);
+        _player.Body.AddAcceleration(_physics.AccelerationRight);
     }
     private void PlayerMoveDown() {
-        _player.Body.AddAcceleration(Vector2.UnitY * _physics.Acceleration);
+        _player.Body.AddAcceleration(_physics.AccelerationDown);
     }
     private void ApplyPhysics() {
-        _player.Body.ApplyKinematics();
-        _player.Body.ResetAcceleration();
-        ResolveCollisions(_player);
+        _player.Body.ApplyPhysics(CheckForCollisions);
     }
-    private void ResolveCollisions(Entity entity) {
-        // Get the coordinates of the tiles at the top left and bottom right of the entity's hitbox.
-        Point topLeft = Shroomworld.DisplayHandler.GetTileCoords(entity.Sprite, i => (int)Math.Floor(i)); // tile pos in map
-        Point bottomRight = topLeft + Shroomworld.DisplayHandler.GetSizeInTiles(entity.Sprite.Texture); //tile coords
-        ClampToMap(ref topLeft);
-        ClampToMap(ref bottomRight);
-        DebugText = $"top left: {topLeft.X}, {topLeft.Y}     bottom right: {bottomRight.X}, {bottomRight.Y}";
-
-        // Find all the tiles within that range.
-        int maxIntersectingTiles = (Shroomworld.DisplayHandler.GetSizeInTiles(entity.Sprite.Texture).Y + 1) * (entity.Sprite.Texture.Width + 1);
-        List<Rectangle> tiles = new List<Rectangle>(capacity: maxIntersectingTiles);
-        for (int x = topLeft.X; x <= bottomRight.X; x++) {
-            for (int y = topLeft.Y; y <= bottomRight.Y; y++) {
-                if (_map[x, y] != TileType.AirId) {
-                    tiles.Add(Shroomworld.DisplayHandler.CalculateTileHitbox(x, y, Shroomworld.TileTypes[_map[x, y]].Texture));
+    /// <summary>
+    /// Checks whether the given <paramref name="hitbox"/> intersects with any solid tiles.
+    /// </summary>
+    /// <param name="hitbox">The hitbox of the entity for which you are checking collisions.</param>
+    /// <returns></returns>
+    private bool CheckForCollisions(Rectangle hitbox) {
+        // Get the tilemap coordinates of the tiles at the top left and bottom right of the entity's hitbox.
+        (Point topLeft, Point bottomRight) corners = _display.GetTileCoords(hitbox);
+        ClampToMap(ref corners.topLeft);
+        ClampToMap(ref corners.bottomRight);
+DebugText += $"top left: {corners.topLeft}\nbottom right: {corners.bottomRight}\n";
+DebugText+=$"Player hitbox position: {_player.Sprite.Hitbox.Location}\n";
+DebugText+=$"Player position: {_player.Sprite.Position}\n";
+        // Check whether the tiles that intersect with the hitbox are solid.
+        TileType tile;
+        for (int x = corners.topLeft.X; x <= corners.bottomRight.X; x++) {
+            for (int y = corners.topLeft.Y; y <= corners.bottomRight.Y; y++) {
+                tile = _tileTypes[_map[x, y]];
+                if (tile.IsSolid && _display.GetTileHitbox(x, y, tile.Texture.Bounds.Size).Intersects(hitbox)) {
+                    return true;
                 }
             }
         }
-        _physics.ResolveCollisions(entity.Body, tiles);
+        return false;
 
-        void ClampToMap(ref Point p) {
-            p.X = Math.Clamp(p.X, 0, _map.Width - 1);
-            p.Y = Math.Clamp(p.Y, 0, _map.Height - 1);
+        /// <summary>
+        /// Clamps the given <see cref="Point"/> to the tile map.
+        /// </summary>
+        /// <param name="point">Tile map coordinates.</param>
+        void ClampToMap(ref Point point) {
+            point.X = Math.Clamp(point.X, 0, _map.Width - 1);
+            point.Y = Math.Clamp(point.Y, 0, _map.Height - 1);
         }
     }
 }
