a808db97291563f4e6c40a73ca76bd39a011267a
W. Walczak <weronikawalczak2005@gmail.com>
2023-03-29 22:21:03 +0100

    feat: add textures

--- Types/ItemType.cs
+++ Types/ItemType.cs
@@ -1,13 +1,17 @@
-﻿namespace Shroomworld;
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Shroomworld;
 
 public class ItemType : IType {
 
     // ----- Properties -----
     public IdData IdData => _idData;
     public Maybe<int> Tile => _tile;
+    public Texture2D Texture => _texture;
 
     // ----- FIelds -----
     private readonly IdData _idData;
+    private readonly Texture2D _texture;
     private readonly Maybe<ToolData> _toolData;
     private readonly Maybe<int> _tile;
     private readonly bool _stackable;
@@ -16,7 +20,7 @@ public class ItemType : IType {
     /// <summary>
     /// Constructor for non-tool items
     /// </summary>
-    public ItemType(IdData idData, bool stackable, Maybe<int> tileType) {
+    public ItemType(IdData idData, Texture2D texture, bool stackable, Maybe<int> tileType) {
         _idData = idData;
         _stackable = stackable;
         _toolData = Maybe.None;
@@ -25,7 +29,7 @@ public class ItemType : IType {
     /// <summary>
     /// Constructor for tool items
     /// </summary>
-    public ItemType(IdData idData, ToolData toolData) {
+    public ItemType(IdData idData, Texture2D texture, ToolData toolData) {
         _idData = idData;
         _toolData = toolData;
         _stackable = false;
