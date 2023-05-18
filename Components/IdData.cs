6eec558e196adc9b344975497011de63b2a575a2
ac1dTigR <artypaws7@gmail.com>
2023-05-18 16:34:16 +0100

    fix: formatting

--- Components/IdData.cs
+++ Components/IdData.cs
@@ -1,24 +1,28 @@
-namespace Shroomworld {
-	/// <summary>Data about the ID of a type.</summary>
-	public class IdData {
+using System;
 
-		// ----- Properties -----
-		public int Id => _id;
-		public string Name => _name;
-		public string PluralName => _pluralName;
+namespace Shroomworld;
+
+/// <summary>
+/// Data about the ID of a type.
+/// </summary>
+public class IdData {
+
+	// ----- Properties -----
+	public int Id => _id;
+	public string Name => _name;
+	public string PluralName => _pluralName;
 
 
-		// ----- Fields -----
-		private readonly int _id;
-		private readonly string _name;
-		private readonly string _pluralName;
+	// ----- Fields -----
+	private readonly int _id;
+	private readonly string _name;
+	private readonly string _pluralName;
 
 
-		// ----- Constructors -----
-		public IdData(int id, string name, string pluralName = null) {
-			_id = id;
-			_name = name;
-			_pluralName = pluralName;
-		}
+	// ----- Constructors -----
+	public IdData(int id, string name, string pluralName = null) {
+		_id = id;
+		_name = name;
+		_pluralName = pluralName;
 	}
 }
